d240a1d6981bb609ba19258fbac1bb57ed56124c
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-03-04 14:04:38 -0300

    first commit

---
+++ curriculum/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Aplicação feita com React Native</Text>
+    </View>
+  )
+}
+
+export default App
